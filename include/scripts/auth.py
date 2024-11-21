7a5b0d0271fb7c607f04adbf6ddbbed5e897516a
ioaviator <kingsolomonifeanyi@gmail.com>
2024-11-21 16:32:23 +0100

    azure storage account authentication

--- include/scripts/auth.py
+++ include/scripts/auth.py
@@ -1,6 +1,7 @@
 import os
 
-from azure.storage.blob import BlobServiceClient
+import pandas as pd
+from azure.storage.blob import BlobServiceClient, ContainerClient
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,8 +9,17 @@ load_dotenv()
 account_name='cdedestorage'
 account_url=f"https://{account_name}.blob.core.windows.net"
 
-blob_url = os.getenv("BLOB_URL")
 account_key = os.getenv("ACCOUNT_KEY")
 
   # Create a BlobServiceClient object
 blob_service_client = BlobServiceClient(account_url=account_url, credential=account_key)
+
+################################
+
+conn_string = os.getenv("CONN_STRING")
+blob_name = 'countries_api.parquet'
+
+container = ContainerClient.from_connection_string(conn_str=conn_string, container_name='raw')
+
+blob_client = container.get_blob_client(blob=blob_name)
+
