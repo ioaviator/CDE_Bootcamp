dec6f0dedaf1c5b8bec008baaac38f1b4cda193a
ioaviator <kingsolomonifeanyi@gmail.com>
2024-11-22 12:41:42 +0100

    fix: cloud deployment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 
 variable "resource_group_name" {
   description = "resource group container for all deployed azure services"
-  default     = "cde_de_resource"
+  default     = "cde_resource"
 }
 
 variable "location" {
