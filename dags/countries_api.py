3e5622c695c6d5aef7fda0713ca47df704bda83b
ioaviator <kingsolomonifeanyi@gmail.com>
2024-11-23 14:09:10 +0100

    dbt airflow integration

--- dags/countries_api.py
+++ dags/countries_api.py
@@ -11,6 +11,8 @@ from airflow.providers.microsoft.azure.operators.data_factory import (
 from airflow.providers.microsoft.azure.sensors.data_factory import (
     AzureDataFactoryPipelineRunStatusSensor,
 )
+from cosmos import DbtDag, DbtTaskGroup, ExecutionConfig, ProfileConfig, ProjectConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
 
 from include.api_connect import connect_to_api
 from include.country_info_to_data_lake import country_info_to_datalake
@@ -22,6 +24,22 @@ sys.path.append(
     os.path.join(os.path.dirname(os.path.dirname(__file__)), "scripts")
 )
 
+DBT_PROJECT_PATH = f"{os.environ['AIRFLOW_HOME']}/dags/dbt/dbt_pipeline"
+profiles_yml = f"{os.environ['AIRFLOW_HOME']}/dags/dbt/dbt_pipeline/profiles.yml"
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    # profile_mapping=profile
+    profiles_yml_filepath=profiles_yml
+)
+
+
+DBT_EXECUTABLE_PATH = f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt"
+
+execution_config = ExecutionConfig(
+    dbt_executable_path=DBT_EXECUTABLE_PATH
+)
 
 default_args = {
     'owner': 'aviator',
@@ -37,6 +55,21 @@ default_args = {
     "factory_name": "cdedatafactory22",
     "resource_group_name": "cde_resource"  
 }
+
+  # dbt_dag = DbtDag(
+  #   project_config=ProjectConfig(DBT_PROJECT_PATH),
+  #   operator_args={"install_deps": True},
+  #   profile_config=profile_config,
+  #   # execution_config=execution_config,
+  #   dag_id="dbt_pipeline",
+  #   default_args=default_args
+  # )
+
+  
+
+
+
+
 
 with DAG(dag_id='countries_api', 
          catchup=False, default_args=default_args) as dag: 
@@ -82,45 +115,21 @@ with DAG(dag_id='countries_api',
         pipeline_name="df_pipeline",
   )
 
+  dbt_tg = DbtTaskGroup(
+      group_id="dbt_transform_data",
+      project_config=ProjectConfig(DBT_PROJECT_PATH),
+      profile_config=profile_config,
+      execution_config=execution_config,
+      operator_args={"install_deps": True},
+      default_args=default_args,
+  )
+
 
   ( start_task 
   >> api_connect 
   >> load_2_data_lake 
   >> transform_data
   >> [language_2_data_lake, country_info_2_data_lake]
-  >>  data_factory >> end_task
+  >>  data_factory >> dbt_tg >> end_task
   )
   
-
-
-
-
-# from cosmos import DbtDag, DbtTaskGroup, ExecutionConfig, ProfileConfig, ProjectConfig
-
-# from cosmos.profiles import PostgresUserPasswordProfileMapping
-
-# profiles_yml = f"{os.environ['AIRFLOW_HOME']}/dags/dbt/dbt_pipeline/profiles.yml"
-
-# profile_config = ProfileConfig(
-#     profile_name="default",
-#     target_name="dev",
-#     # profile_mapping=profile
-#     profiles_yml_filepath=profiles_yml
-# )
-
-
-# DBT_EXECUTABLE_PATH = f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt"
-
-# execution_config = ExecutionConfig(
-#     dbt_executable_path=DBT_EXECUTABLE_PATH
-# )
-
-# dbt_dag = DbtDag(
-#   project_config=ProjectConfig("/usr/local/airflow/dags/dbt/dbt_pipeline"),
-#   operator_args={"install_deps": True},
-#   profile_config=profile_config,
-#   # execution_config=execution_config,
-#   dag_id="dbt_pipeline",
-#   default_args=default_args
-# )
-
