004bb7da710510c4beb706db5eec476a5c2d7b2f
ioaviator <kingsolomonifeanyi@gmail.com>
2024-11-27 14:20:03 +0100

    code refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,11 +50,6 @@ module "data_factory_blob_storage" {
   linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.cdeblobstoragels.name
 }
 
-module "data_factory_postgres" {
-  source = "./data_factory_postgres"
-  data_factory_id = azurerm_data_factory.cdedatafactory.id
-}
-
 
 module "container_registry" {
   source = "./container_registry"
